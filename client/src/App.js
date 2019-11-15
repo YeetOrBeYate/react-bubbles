c19260e5a1a7eaca3ffb567f20a0d44b0c9d0ba5
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-15 12:10:53 -0500

    Got the authenticated get request to retrive the color list from the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute"
 import "./App.css"
 
 import Login from "./components/Login";
@@ -7,15 +9,27 @@ import Login from "./components/Login";
 
 function App() {
   return (
-    <Router>
+    
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <nav>
+    <Link to= "/login">Login</Link>
+    <Link to="/protected">Friends</Link>
+    </nav>
+    
+
+    <Switch>
+      <Route path = '/login' component={Login}/>
+      <PrivateRoute path = "/protected">
+        <Route path = "/protected" component={BubblePage}/>
+      </PrivateRoute>
+      <Route component={Login}/>
+    </Switch>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
       </div>
-    </Router>
+    
   );
 }
 
