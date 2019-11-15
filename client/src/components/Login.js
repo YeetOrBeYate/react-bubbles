4ace6967080a8ef4d3300e52351bed3b5946550d
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-15 11:51:29 -0500

    Created the post request to store the token in localstorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,6 +10,9 @@ const Login = () => {
     username: "",
     password: ""
   })
+  const [loading, setLoading] = React.useState(false);
+  const [message, setMessage] = React.useState("");
+  const [display, setDisplay] = React.useState(false);
 
   const handleChange = (e)=>{
     setInput({...input, [e.target.name]: e.target.value})
@@ -17,12 +20,29 @@ const Login = () => {
 
   const handleLogin = (e)=>{
     e.preventDefault();
-    console.log(input);
+    
+    setLoading(true);
+    axios.post("http://localhost:5000/api/login", input)
+      .then((res)=>{
+        console.log(res)
+        setMessage("Youre In!")
+        setDisplay(true);
+        localStorage.setItem("token", res.data.payload);
+        console.log(localStorage)
+      })
+      .catch((err)=>{
+        console.log(err)
+        setMessage("Opsie!, Somting wen wongg!");
+        setDisplay(true);
+      })
+      .finally(()=>{
+        setLoading(false);
+      })
   }
 
   return (
     <div className= "Form">
-      <h1>Welcome to the Bubble App!</h1>
+      {!loading ? <h1>Welcome to the Bubble App!</h1> : <h1>Loading...</h1>}
       <form>
         <div>
           <label>Username:</label>
@@ -33,6 +53,7 @@ const Login = () => {
           <input name= "password" value = {input.password} onChange={handleChange}/>
         </div>
         <button onClick={handleLogin}>Login!</button>
+        {!display ? <h1></h1> : <h1>{message}</h1>}
       </form>
     </div>
   );
